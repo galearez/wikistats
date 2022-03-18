67d250e91b286c9ccf2dc74ade61a500b34f2f11
Josep <51718252+galearez@users.noreply.github.com>
2022-03-17 22:31:37 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<main>
+  <h1>Wikipedia Stats</h1>
+  <form action="/">
+    <input type="search" name="search" placeholder="e.g. Albert Einstein" />
+    <button type="submit">Search</button>
+  </form>
+</main>
