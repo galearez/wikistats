3096ecf3f843daf0f787ee4125422d0d267edb3f
Josep <51718252+galearez@users.noreply.github.com>
2022-03-18 22:57:32 -0600

    Basic form behavior

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,32 @@
-<main>
+<script lang="ts">
+  // this variable will contain the value to make the request
+  let query: string = undefined;
+  // this variable control the input field for the search form
+  let searchValue: string;
+  // function to follow the wikipedia query format
+  function regularize(s: string): string {
+    if (s === undefined || s === '') {
+      return undefined;
+    }
+
+    return s.toLocaleLowerCase().replace(/\s+/g, '_');
+  }
+</script>
+
+<header>
   <h1>Wikipedia Stats</h1>
-  <form action="/">
-    <input type="search" name="search" placeholder="e.g. Albert Einstein" />
+  <form
+    action="/"
+    on:submit|preventDefault={() => {
+      query = regularize(searchValue);
+    }}
+  >
+    <input
+      type="search"
+      name="search"
+      placeholder="Enter some wikipedia page"
+      bind:value={searchValue}
+    />
     <button type="submit">Search</button>
   </form>
-</main>
+</header>
