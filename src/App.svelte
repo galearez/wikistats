73031e4cc8be285e94fdfa15102ade8e68011f2c
galearez <51718252+galearez@users.noreply.github.com>
2022-04-14 20:31:42 -0600

    implemented the 'Router'
    With the addition of the 'Router' I don't need to have so many values
    inside my store, because mosts thing will be managed using the router,
    so the code in 'App' is now cleaner. After finnishing the implementation,
    I will have less components in the App  and less global values inside store.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import { setContext } from 'svelte';
-  import { apiCtx, home, headerClass } from './app/store';
-  import HomeForm from './lib/HomeForm.svelte';
-  import PageStats from './lib/PageStats.svelte';
-  import SearchResults from './lib/SearchResults.svelte';
+  import { apiCtx } from './app/store';
+  import routes from './app/routes';
+  import Router from 'svelte-spa-router';
+  import Layout from './lib/Layout.svelte';
 
   // since this portion of the URL repeats on all of the reqs I hopted to make a
   // context to be passed to the children components
@@ -12,43 +12,8 @@
   setContext(apiCtx, {
     api: apiURL,
   });
-
-  // this store value will control which component is rendered
-  let isHome: boolean;
-  home.subscribe((value) => (isHome = value));
-
-  // this will do something
-  let hClass: any;
-  headerClass.subscribe((value) => (hClass = value));
 </script>
 
-<main>
-  {#if isHome}
-    <header class={hClass}>
-      <HomeForm />
-    </header>
-    <SearchResults />
-  {:else}
-    <PageStats />
-  {/if}
-</main>
-
-<style>
-  main {
-    width: 100vw;
-    height: 100vh;
-  }
-  .initial {
-    position: absolute;
-    top: 20%;
-    left: 50%;
-    transform: translateX(-50%);
-    width: min(550px, 100%);
-  }
-  .results {
-    position: static;
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-  }
-</style>
+<Layout>
+  <Router {routes} />
+</Layout>
