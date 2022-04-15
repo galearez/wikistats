338c0a47ca378b6b0e35cf869240e83350bbb807
galearez <51718252+galearez@users.noreply.github.com>
2022-04-14 20:24:15 -0600

    added comments and cleaned code

--- src/routes/Page.svelte
+++ src/routes/Page.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
   import { getContext, onMount } from 'svelte';
   import { apiCtx, page } from '../app/store';
-  import { querystring } from 'svelte-spa-router';
 
+  // this variable holds the title of the page, it must be exactly title of one existen wikipedia page
   let pageTitle: string;
+  // once the component is mounted, it will get the value passed in the URL
   onMount(() => page.set(params.title));
   page.subscribe((value) => (pageTitle = value));
 
+  // this is the url string used for the requests
   const { api } = getContext(apiCtx);
 
+  // function to get the page thumnail
   async function thumbnail(query: string): Promise<string> {
     let source = '';
     await fetch(
@@ -24,6 +27,7 @@
     return source;
   }
 
+  // function to get the title, the date of the last edit and the lenght
   async function info(
     query: string
   ): Promise<{ title: string; touched: string; length: string }> {
@@ -39,6 +43,7 @@
     return { title, touched, length };
   }
 
+  // function to get the views of the last month
   async function views(query: string): Promise<number> {
     let views = 0;
     await fetch(`${api}query&prop=pageviews&titles=${query}`)
@@ -57,6 +62,7 @@
     return views;
   }
 
+  // function to get see in how many languages is available
   async function langs(query: string): Promise<number> {
     let langLinks = 0;
     await fetch(`${api}query&prop=langlinks&lllimit=max&titles=${query}`)
@@ -71,12 +77,13 @@
     return langLinks;
   }
 
+  // this parameters correspond to the ones passed by the url, this array is managed by the router
   export let params: any = {};
 </script>
 
 {#if pageTitle !== undefined}
   {#await thumbnail(pageTitle) then src}
-    <img {src} alt="" />
+    <img {src} alt={`${pageTitle}.png`} />
   {/await}
 
   {#await info(pageTitle) then { title, touched, length }}
