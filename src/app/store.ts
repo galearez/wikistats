0c4b68d5057c96acbed8f1b33556943dccd8bf16
galearez <51718252+galearez@users.noreply.github.com>
2022-04-14 20:46:47 -0600

    removed unused componend and store values

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,7 @@
 import { readable, writable } from 'svelte/store';
 
-const query = writable<string>(undefined);
-const page = writable<string>(undefined);
 const apiCtx = readable<Symbol>(undefined, () => {
   Symbol();
 });
-const home = writable<boolean>(true);
 
-type HeaderClasses = 'initial' | 'results';
-const headerClass = writable<HeaderClasses>('initial');
-
-export { query, apiCtx, page, home, headerClass };
+export { apiCtx };
