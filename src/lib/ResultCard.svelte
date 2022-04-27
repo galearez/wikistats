1d67c7f6d6a0144e0b4e3065f8713954c0e965cb
Josep Galearez <51718252+galearez@users.noreply.github.com>
2022-04-26 22:17:06 -0600

    made result card lighter to load

--- src/lib/ResultCard.svelte
+++ src/lib/ResultCard.svelte
@@ -6,21 +6,7 @@
   // this is the api URLv
   const { api } = getContext(apiCtx);
 
-  async function getDescription(query: string): Promise<string> {
-    let description = '';
-    await fetch(`${api}query&prop=description&titles=${query}`)
-      .then((res) => res.json())
-      .then((data) => {
-        const id = Object.keys(data.query.pages);
-        ({ description } = data.query.pages[id[0]]);
-      })
-      .catch(console.error);
-
-    return description;
-  }
-
   export let title: string;
-  export let link: string;
 </script>
 
 <div class="card">
@@ -30,13 +16,9 @@
       push(`/p/${title}`);
     }}
   >
-    <span>See stats of</span>
     <h2>
       {title}
     </h2>
-    {#await getDescription(title) then description}
-      <p>{description}</p>
-    {/await}
     <p />
   </div>
   <div class="extern-link">
